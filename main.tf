380def3087ca8eaa0c177f401ed03d3e8915bd66
Jonathan Seth Mainguy <jon@soh.re>
2025-04-04 13:56:39 -0400

    fix: change to using vars

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "oci_core_instance" "vm1" {
-  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
+  availability_domain = var.availability_domain
   compartment_id      = var.compartment_ocid
   shape               = "VM.Standard.E2.1.Micro"
 
@@ -9,18 +9,18 @@ resource "oci_core_instance" "vm1" {
 
   source_details {
     source_type = "image"
-    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxmirlvvwnhwrg4lcfcwqd327nhkduwd6xyniamqnjjq2wq62c6wq"
+    source_id   = var.image_source_ocid
   }
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
   }
 
-  display_name = "crossplane-vm1"
+  display_name = "vm1.soh.re"
 }
 
 resource "oci_core_instance" "vm2" {
-  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0].name
+  availability_domain = var.availability_domain
   compartment_id      = var.compartment_ocid
   shape               = "VM.Standard.E2.1.Micro"
 
@@ -30,16 +30,12 @@ resource "oci_core_instance" "vm2" {
 
   source_details {
     source_type = "image"
-    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxmirlvvwnhwrg4lcfcwqd327nhkduwd6xyniamqnjjq2wq62c6wq"
+    source_id   = var.image_source_ocid
   }
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
   }
 
-  display_name = "crossplane-vm2"
-}
-
-data "oci_identity_availability_domains" "ADs" {
-  compartment_id = var.tenancy_ocid
+  display_name = "vm2.soh.re"
 }
