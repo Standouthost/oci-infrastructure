bfc1516768f5c3e5c63a5489ba5150a9d268ce2a
Jonathan Seth Mainguy <jon@soh.re>
2025-04-04 13:19:31 -0400

    fix: hardcode id

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "oci_core_instance" "vm1" {
 
   source_details {
     source_type = "image"
-    source_id   = data.oci_core_images.ubuntu_2404.images[0].id
+    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxmirlvvwnhwrg4lcfcwqd327nhkduwd6xyniamqnjjq2wq62c6wq"
   }
 
   metadata = {
@@ -30,7 +30,7 @@ resource "oci_core_instance" "vm2" {
 
   source_details {
     source_type = "image"
-    source_id   = data.oci_core_images.ubuntu_2404.images[0].id
+    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxmirlvvwnhwrg4lcfcwqd327nhkduwd6xyniamqnjjq2wq62c6wq"
   }
 
   metadata = {
@@ -42,14 +42,4 @@ resource "oci_core_instance" "vm2" {
 
 data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
-}
-
-data "oci_core_images" "ubuntu_2404" {
-  compartment_id = var.compartment_ocid
-
-  # OCI requires that if you filter by display_name, you must not filter by anything else.
-  display_name   = "Canonical-Ubuntu-24.04-Minimal-2025.03.28-0"
-
-  sort_by        = "TIMECREATED"
-  sort_order     = "DESC"
 }
