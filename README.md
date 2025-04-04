e5e0e3c546eafea42a73144486f7f985aea5a42d
Jonathan Seth Mainguy <jon@soh.re>
2025-04-04 12:27:38 -0400

    fix: stuff

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# oci-infrastructure
-OCI Infrastructure as code
+# Oracle Cloud Free Tier VM Deployment
+
+This Terraform configuration provisions **two x86 Oracle Linux VMs** under the Oracle Free Tier limits using the `VM.Standard.E2.1.Micro` shape.
+
+## Prerequisites
+
+- Oracle Cloud account
+- SSH keypair
+- A subnet in your compartment
+
+## Variables
+
+Pass these in via a `terraform.tfvars` file or set them via Crossplane's Workspace `vars`.
+
+## Outputs
+
+This module does not define any outputs.
+
+## Usage (Crossplane)
+
+Reference this Git repo in a `Workspace`:
+
+```yaml
+module:
+  url: git::https://github.com/yourname/oracle-vms-tf.git//?ref=main
+```
--- variables.tf
+++ variables.tf
@@ -1,8 +1,40 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "compartment_ocid" {}
-variable "subnet_ocid" {}
-variable "ssh_public_key" {}
+variable "tenancy_ocid" {
+  description = "OCI Tenancy OCID"
+  type        = string
+}
+
+variable "user_ocid" {
+  description = "OCI User OCID"
+  type        = string
+}
+
+variable "fingerprint" {
+  description = "API Key fingerprint"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to private API key"
+  type        = string
+}
+
+variable "region" {
+  description = "OCI Region"
+  type        = string
+}
+
+variable "compartment_ocid" {
+  description = "Compartment OCID to deploy into"
+  type        = string
+}
+
+variable "subnet_ocid" {
+  description = "Subnet OCID to attach instances"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key to inject into instance metadata"
+  type        = string
+}
+
